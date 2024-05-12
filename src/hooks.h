6df47a81a991a733006d4c27adbfc672e7c828bc
nightfallstorm <109926124+Nightfallstorm@users.noreply.github.com>
2024-05-11 22:12:31 -0400

    Remove applying of bounds
    This should improve the ability to move the cursor outside the menu for switching back to controller mode

--- src/hooks.h
+++ src/hooks.h
@@ -21,10 +21,6 @@ namespace hooks
 			a_y += lengthDiff / 2.0f;
 			a_y = a_y * DPI;
 
-			// Apply bounds after DPI calculation
-			a_x = min(a_coords->maxX, max(a_x, a_coords->minX));
-			a_y = min(a_coords->maxY, max(a_y, a_coords->minY));
-
 			// Apply smoothing			
 			std::pair newCoords = smoother->SmoothCoords(a_coords, a_x, a_y);
 
@@ -44,6 +40,24 @@ namespace hooks
 
 		static void Install() {
 			REL::Relocation<std::uintptr_t> target{ REL::Offset(0x6B8320) };
+			std::uintptr_t function = target.address() + 0x3E4;
+
+			// Remove bounds from the original function. We will do bounds after calculating DPI
+			std::uintptr_t XpercentCapStart = target.address() + 0x399;
+			std::uintptr_t XpercentCapEnd = target.address() + 0x3AD;
+			REL::safe_fill(XpercentCapStart, REL::NOP, XpercentCapEnd - XpercentCapStart);
+			std::uintptr_t YpercentCapStart = target.address() + 0x3B5;
+			std::uintptr_t YpercentCapEnd = target.address() + 0x3C9;
+			REL::safe_fill(YpercentCapStart, REL::NOP, YpercentCapEnd - YpercentCapStart);
+
+			// Hook mouse coords update function
+			pstl::write_thunk_call<MouseCoordsUpdateHook>(function);
+		}
+	};
+
+	struct RemoveCoordsCap {
+		static void Install() {
+			REL::Relocation<std::uintptr_t> target{ REL::Offset(0xF33CD0) };
 			std::uintptr_t function = target.address() + 0x3E4;
 
 			// Remove bounds from the original function. We will do bounds after calculating DPI
